f39a7e6d877b6bb86803da8bcc5b3450e04c15a6
Nathan Grilliot <nrgrill2003@gmail.com>
2023-02-25 15:22:47 -0500

    Create Calendar HTML and add route

--- ManageMe/website/auth.py
+++ ManageMe/website/auth.py
@@ -64,3 +64,8 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
+
+@auth.route('/calendar', methods=['GET', 'POST'])
+def calendar():
+
+    return render_template("calendar.html", user=current_user)
