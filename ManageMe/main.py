39b32b95d09d13b5a5d5e9159802e397fb692f29
Nathan Grilliot <nrgrill2003@gmail.com>
2023-02-25 14:31:11 -0500

    fix main

--- ManageMe/main.py
+++ ManageMe/main.py
@@ -3,8 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.run(debug=True)
-=======
     app.run()
->>>>>>> d893e1035d60aafa1a671014178e10a59fbd4b36
