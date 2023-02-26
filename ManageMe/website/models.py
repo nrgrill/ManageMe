4beb0f4ac443640722050f5d4121acfb2bfc08db
Eli Fouts <116454864+elifouts@users.noreply.github.com>
2023-02-26 11:48:55 -0500

    oopy woopy

--- ManageMe/website/models.py
+++ ManageMe/website/models.py
@@ -33,3 +33,5 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     tasks = db.relationship('Task')
+    stimes = db.relationship('StartTime')
+    etimes = db.relationship('EndTime')
